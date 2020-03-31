116778bae2baacfbbe79e6319f19238e5934cab5
SuneelKumar Vanapalli <suneel.vanapalli@gmail.com>
2020-03-31 17:01:12 +1100

    course project basics - setting up components

--- RecipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipesProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('test', 'test', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1200,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
+    new Recipe('test', 'test', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1200,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
+    new Recipe('test', 'test', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1200,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
--- RecipesProject/src/app/shopping-list/shopping-list.component.ts
+++ RecipesProject/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 100),
+    new Ingredient('Tomatoes', 100),
+    new Ingredient('Ginger', 5)
+  ];
   constructor() { }
 
   ngOnInit() {
