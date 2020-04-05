20e7481d2b9ad375e033452b0415dbcbee0a53d7
SuneelKumar Vanapalli <suneel.vanapalli@gmail.com>
2020-04-05 10:21:41 +1000

    hostbinding

--- RecipesProject/src/app/shared/basicHighlight.directive.ts
+++ RecipesProject/src/app/shared/basicHighlight.directive.ts
@@ -1,17 +1,26 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { EventEmitter } from 'protractor';
 
 @Directive({
     selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit {
-
+@HostBinding('style.backgroundColor') backgroundColor = 'transparent';
   constructor(private element: ElementRef, private renderer: Renderer2) {
 
   }
 
     ngOnInit(): void {
         this.element.nativeElement.style.backgroundColor = 'green';
-        this.renderer.setStyle(this.element.nativeElement, 'color', 'blue');
+    }
+
+    @HostListener('mouseenter') mouseover(eventData: Event) {
+        //this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'blue');
+        this.backgroundColor = 'blue';
+    }
+
+    @HostListener('mouseleave') mouseleave(eventData: Event) {
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'transparent');
     }
 
 }
