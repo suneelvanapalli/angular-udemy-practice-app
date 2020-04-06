5d3ed5d196ae2ce223cbe8013ec9afc711bb9175
SuneelKumar Vanapalli <suneel.vanapalli@gmail.com>
2020-04-06 15:48:54 +1000

    closing the drop down from anywhere pending

--- RecipesProject/src/app/shared/dropdown.directive.ts
+++ RecipesProject/src/app/shared/dropdown.directive.ts
@@ -1,19 +1,33 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
+@Directive({
+  selector: '[appDropdown]'
+})
 export class DropdownDirective {
-  // tslint:disable-next-line: no-inferrable-types
-  toggleOpenMode: boolean = false;
-  constructor(private element: ElementRef, private renderer: Renderer2) {}
-
-  @HostListener('click') mouseover(eventData: Event) {
-    this.toggleOpenMode = !this.toggleOpenMode;
-    if (this.toggleOpenMode) {
-      this.renderer.addClass(this.element.nativeElement, 'open');
-    } else {
-      this.renderer.removeClass(this.element.nativeElement, 'open');
-    }
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  constructor(private elRef: ElementRef) { }
+  // export class DropdownDirective {
+  //   // tslint:disable-next-line: no-inferrable-types
+  //   toggleOpenMode: boolean = false;
+  //   constructor(private element: ElementRef, private renderer: Renderer2) {}
+
+  //   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+  //     this.toggleOpenMode = !this.toggleOpenMode;
+  //     this.toggleOpenMode = this.element.nativeElement.contains(event.target)
+  //       ? !this.toggleOpenMode
+  //       : false;
+
+  //     if (this.toggleOpenMode) {
+  //       this.renderer.addClass(this.element.nativeElement, 'open');
+  //     } else {
+  //       this.renderer.removeClass(this.element.nativeElement, 'open');
+  //     }
+  //   }
+  // }
 }
