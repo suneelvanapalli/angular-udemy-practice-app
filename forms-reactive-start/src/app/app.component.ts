759e4cca8b4de8eafe63e43f1cfcdfc0be99e7c3
Suneel Kumar Vanapalli <A30001344@agl.com.au>
2020-04-16 16:59:51 +1000

    reactive form practice

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders = ["male", "female"];
+  signupForm: FormGroup;
+  forbiddenUserNames: ["test", "123"];
+
+  get controls() {
+    return (this.signupForm.get("hobbies") as FormArray).controls;
+  }
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl("s@s.com", [
+          Validators.required,
+          Validators.email,
+        ]),
+      }),
+      gender: new FormControl("male"),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  onAddHobby() {
+    const newHobbyControl = new FormControl(null, Validators.required);
+    let formarr = this.signupForm.get("hobbies") as FormArray;
+    formarr.push(newHobbyControl);
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: false };
+    }
+    return null;
+  }
 }
